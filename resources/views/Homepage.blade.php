867ad2846d871fad4e30b0010638428a6df72e55
Josua Malquiel T. Araracap <85546169+userg19@users.noreply.github.com>
2024-01-16 15:45:54 +0800

    DelexWebpage
    v0.0.1

---
+++ resources/views/Homepage.blade.php
@@ -0,0 +1,82 @@
+<html lang="en">
+   <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Document</title>
+      <link href="/css/mainpagenavbar.css" rel="stylesheet">
+   </head>
+  <body>
+    <header>
+      <nav class="navbar">
+        <a href="http://127.0.0.1:8000/"><img src="/css/image/header-logo-nav.png" alt="company logo"></a>
+        <a href="#">LOGIN</a>
+        <a href="#">CONTACT US</a>
+        <a href="#">SEARCH</a>
+        <div class="dropdown">
+          <button class="dropbtn">ABOUT US
+            <i class="arrow down"></i>
+            <i class="fa fa-caret-down"></i>
+          </button>
+          <div class="dropdown-content">
+            <a href="#">DELEX THROUGH THE YEARS</a>
+            <a href="#">OUR LEADERSHIP</a>
+            <a href="#">OUR CORPORATE DIRECTION</a>
+            <a href="#">CSA PROGRAMS</a>
+            <a href="#">FACT SHEET</a>
+          </div>
+        </div>
+        <div class="dropdown">
+          <button class="dropbtn">OUR PRODUCTS
+            <i class="arrow down"></i> 
+            <i class="fa fa-caret-down"></i>
+          </button>
+          <div class="dropdown-content">
+            <a href="#">CRITICAL CARE</a>
+            <a href="#">ANTI INFECTIVE</a>
+            <a href="#">ANESTHESIA</a>
+            <a href="#">MEDICAL DEVICES</a>
+          </div>
+        </div>
+        <div class="dropdown">
+          <button class="dropbtn">DISTRIBUTION
+            <i class="arrow down"></i>
+            <i class="fa fa-caret-down"></i>
+          </button>
+          <div class="dropdown-content">
+            <a href="#">MANUFACTURERS</a>
+            <a href="#">OUR PARTNERS</a>
+          </div>
+        </div>
+        <div class="dropdown">
+          <button class="dropbtn">FORUMS
+            <i class="arrow down"></i>
+            <i class="fa fa-caret-down"></i>
+          </button>
+          <div class="dropdown-content">
+            <a href="#">ICU FORUM PROGRAM</a>
+            <a href="#">INTRAFEN FORUM</a>
+            <a href="#">TOTILAC FORUM</a>
+          </div>
+        </div>
+          <a href="#">DELEX UNIVERSITY</a>
+        <div class="dropdown">
+          <button class="dropbtn">I AM DELEX
+            <i class="arrow down"></i>
+            <i class="fa fa-caret-down"></i>
+          </button>
+          <div class="dropdown-content">
+            <a href="#">CAREER OPPORTUNITIES</a>
+            <a href="#">PROMOTIONS</a>
+            <a href="#">ON THE JOB TRAINING</a>
+            <a href="#">CAREER FAIR</a>
+            <a href="#">DELEX ASSESSMENT CENTER</a>
+          </div>
+        </div>
+      </nav>
+    </header> 
+      <div class="Home-Banner">
+        <img src="/css/image/Delex Post.jpg" alt="homebanner">
+      </div>
+  </body>
+</html>
